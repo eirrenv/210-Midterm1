6922bc6a4ac5788e25086217dd66fe66fcf92131
Eirren <edv2019@gmail.com>
2026-03-18 18:19:34 -0700

    10 min commit, commenting

--- Midterm1_1.cpp
+++ Midterm1_1.cpp
@@ -1,45 +1,45 @@
 #include <iostream>
 using namespace std;
 
-const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
+const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20; // const variable calls for min and max number and list size
 
-class DoublyLinkedList {
-private:
-    struct Node {
-        int data;
-        Node* prev;
-        Node* next;
-        Node(int val, Node* p = nullptr, Node* n = nullptr) {
-            data = val; 
+class DoublyLinkedList { // start of DoublyLinkedList class
+private: // contains data that should not be edited unless by a public function call
+    struct Node { // creating struct for Nodes in linked list
+        int data; // data inside linked list
+        Node* prev; // pointer to hold prev node location
+        Node* next; // pointer for next node location
+        Node(int val, Node* p = nullptr, Node* n = nullptr) { // complete parameter constructor for a Node
+            data = val;
             prev = p;
             next = n;
         }
     };
 
-    Node* head;
-    Node* tail;
+    Node* head; // pointer for front of linked list
+    Node* tail; // pointer for end of linked list
 
-public:
-    DoublyLinkedList() { head = nullptr; tail = nullptr; }
+public: // public information for class that can be used in main() function
+    DoublyLinkedList() { head = nullptr; tail = nullptr; } // default constructor for DoublyLinkedList class
 
-    void insert_after(int value, int position) {
-        if (position < 0) {
+    void insert_after(int value, int position) { // insert Node after specific position in linked list
+        if (position < 0) { // if invalid position < 0 is used, print error msg and exit
             cout << "Position must be >= 0." << endl;
             return;
         }
 
-        Node* newNode = new Node(value);
-        if (!head) {
+        Node* newNode = new Node(value); // creating new Node for linked list
+        if (!head) { // if linked list is empty, set head and tail to pointer of new Node
             head = tail = newNode;
             return;
         }
 
-        Node* temp = head;
-        for (int i = 0; i < position && temp; ++i)
+        Node* temp = head; // create new temp Node to start from head's position pointer
+        for (int i = 0; i < position && temp; ++i) // iterate through linked list to find location
             temp = temp->next;
 
-        if (!temp) {
-            cout << "Position exceeds list size. Node not inserted.\n";
+        if (!temp) { // if temp ends as a nullptr, the position is out of the linked list bounds
+            cout << "Position exceeds list size. Node not inserted.\n"; // prints error message, deletes node to insert, ends function
             delete newNode;
             return;
         }
