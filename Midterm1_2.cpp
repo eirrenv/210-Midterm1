3858b756720fd8e5cca8c3dff472f4bf3d8918a2
Eirren <edv2019@gmail.com>
2026-03-18 20:25:48 -0700

    10 minute commit

--- Midterm1_2.cpp
+++ Midterm1_2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
@@ -6,10 +7,10 @@ const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
 class DoublyLinkedList {
 private:
     struct Node {
-        int data;
+        string data;
         Node* prev;
         Node* next;
-        Node(int val, Node* p = nullptr, Node* n = nullptr) {
+        Node(string val, Node* p = nullptr, Node* n = nullptr) {
             data = val; 
             prev = p;
             next = n;
@@ -22,7 +23,7 @@ private:
 public:
     DoublyLinkedList() { head = nullptr; tail = nullptr; }
 
-    void insert_after(int value, int position) {
+    void insert_after(string value, int position) {
         if (position < 0) {
             cout << "Position must be >= 0." << endl;
             return;
@@ -53,7 +54,7 @@ public:
         temp->next = newNode;
     }
 
-    void delete_val(int value) {
+    void delete_val(string value) {
         if (!head) return;
 
         Node* temp = head;
@@ -113,7 +114,7 @@ public:
         delete temp;
     }
 
-    void push_back(int v) {
+    void push_back(string v) {
         Node* newNode = new Node(v);
         if (!tail)
             head = tail = newNode;
@@ -124,7 +125,7 @@ public:
         }
     }
     
-    void push_front(int v) {
+    void push_front(string v) {
         Node* newNode = new Node(v);
         if (!head)
             head = tail = newNode;
@@ -201,10 +202,43 @@ public:
         }
         cout << endl;
     }
+
+    void printLine() {
+        Node* current = head;
+        if (!current) {
+            cout << "List is empty." << endl;
+            return;
+        }
+        while (current) {
+            cout << "\t" << current->data << " joins the line" << endl;
+            current = current->next;
+        }
+        cout << endl;
+    }
 };
 
 int main() {
+
+    DoublyLinkedList* line = new DoublyLinkedList();
     int value = value = rand() % 100 + 1;
+
+    ifstream fin("names.txt");
+
+    if (!fin) {
+        cout << "Error opening file!" << endl;
+        return 1;
+    }
     
+    string name;
+    fin >> name;
+
+    cout << "Store opens:" << endl;
+    for (int i = 0; i < 5; ++i) {
+        line->push_front(name);
+        fin >> name;
+    }
+    line->printLine();
+
+    fin.close();
     return 0;
 }
